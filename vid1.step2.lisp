9e3cdc28538acdbd1f041d33bb59480443fe81ee
stacksmith <stacksmith@users.noreply.github.com>
2016-10-18 19:12:08 -0700

    Update vid1.step2.lisp

--- vid1.step2.lisp
+++ vid1.step2.lisp
@@ -5,7 +5,7 @@
 ;;;
 ;;(ql:quickload :cepl.sdl2)(ql:quickload :cepl.examples)(in-package cepl.examples)(cepl:repl)
 
-(in-package #:cepl-learn)
+(in-package #:cepl.examples)
 (defparameter *running* nil)
 ;; *quad* is a list compatible with g-pt format (vec-3 pos and a vec-2 tex)
 (defparameter *quad* (list (list (v! -0.5  0.5 0.0) (v! 0.0 0.0))
